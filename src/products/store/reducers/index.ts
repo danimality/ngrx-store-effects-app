9a84c2153ba9f817380e02dfe2b26cb913bc1868
Dan Wheatley <dan@tassomai.com>
2017-12-16 15:42:54 +0000

    adds toppings reducers

--- src/products/store/reducers/index.ts
+++ src/products/store/reducers/index.ts
@@ -3,13 +3,16 @@ import {
   createFeatureSelector,
 } from "@ngrx/store";
 import * as fromPizzas from "./pizzas.reducer";
+import * as fromToppings from "./toppings.reducer";
 
 export interface ProductsState {
   pizzas: fromPizzas.PizzaState;
+  toppings: fromToppings.ToppingsState;
 }
 
 export const reducers: ActionReducerMap<ProductsState> = {
-  pizzas: fromPizzas.reducer
+  pizzas: fromPizzas.reducer,
+  toppings: fromToppings.reducer
 };
 
 // top level selectors
