f0ffc81e9d29a5d242285e99f08953c7a429bafa
Dan Wheatley <dan@tassomai.com>
2017-12-17 12:50:57 +0000

    adds toppings selectors in component

---
+++ src/products/store/selectors/toppings.selectors.ts
@@ -0,0 +1,22 @@
+import { createSelector } from "@ngrx/store";
+import * as fromFeature from "../reducers";
+import * as fromToppings from "../reducers/toppings.reducer";
+import * as fromRoot from "../../../app/store";
+
+export const getToppingsState = createSelector(
+  fromFeature.getProductsState,
+  (state: fromFeature.ProductsState) => state.toppings
+);
+
+export const getToppingEntities = createSelector(
+  getToppingsState,
+  fromToppings.getToppingEntities
+);
+
+export const getAllToppings = createSelector(getToppingEntities, entities => {
+  return Object.keys(entities).map(id => entities[parseInt(id, 10)]);
+});
+
+export const getToppingsLoaded = createSelector(getToppingsState, fromToppings.getToppingsLoaded);
+export const getToppingsLoading = createSelector(getToppingsState, fromToppings.getToppingsLoading);
+
