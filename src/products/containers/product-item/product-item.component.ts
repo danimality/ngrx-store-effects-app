ae98a7240b979fb440d670801423b27701399d7d
Dan Wheatley <dan@tassomai.com>
2017-12-17 23:10:18 +0000

    adds visualise toppings round trip

--- src/products/containers/product-item/product-item.component.ts
+++ src/products/containers/product-item/product-item.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { Store } from "@ngrx/store";
 import { Observable } from "rxjs/Observable";
-import * as fromStore from '../../store';
+import { tap } from "rxjs/operators";
+import * as fromStore from "../../store";
 
-import { Pizza } from '../../models/pizza.model';
+import { Pizza } from "../../models/pizza.model";
 
-import { Topping } from '../../models/topping.model';
+import { Topping } from "../../models/topping.model";
 
 @Component({
-  selector: 'product-item',
-  styleUrls: ['product-item.component.scss'],
+  selector: "product-item",
+  styleUrls: ["product-item.component.scss"],
   template: `
     <div 
       class="product-item">
@@ -21,42 +22,45 @@ import { Topping } from '../../models/topping.model';
         (update)="onUpdate($event)"
         (remove)="onRemove($event)">
         <pizza-display
-          [pizza]="visualise">
+          [pizza]="visualise$ | async">
         </pizza-display>
       </pizza-form>
     </div>
-  `,
+  `
 })
 export class ProductItemComponent implements OnInit {
   pizza$: Observable<Pizza>;
-  visualise: Pizza;
+  visualise$: Observable<Pizza>;
   toppings$: Observable<Topping[]>;
 
-  constructor(private store: Store<fromStore.ProductsState>
-  ) {}
+  constructor(private store: Store<fromStore.ProductsState>) {}
 
   ngOnInit() {
-    this.store.dispatch(new fromStore.LoadToppings());
-    this.pizza$ = this.store.select(fromStore.getSelectedPizza);
+    this.pizza$ = this.store.select(fromStore.getSelectedPizza).pipe(
+      tap((pizza: Pizza) => {
+        // '/products/2' or '/products/new
+        const pizzaExists = !!(pizza && pizza.toppings);
+        const toppings = pizzaExists
+          ? pizza.toppings.map(topping => topping.id)
+          : [];
+        this.store.dispatch(new fromStore.VisualiseToppings(toppings));
+      })
+    );
     this.toppings$ = this.store.select(fromStore.getAllToppings);
+    this.visualise$ = this.store.select(fromStore.getPizzaVisualised);
   }
 
   onSelect(event: number[]) {
-
+    this.store.dispatch(new fromStore.VisualiseToppings(event));
   }
 
-  onCreate(event: Pizza) {
+  onCreate(event: Pizza) {}
 
-  }
-
-  onUpdate(event: Pizza) {
-
-  }
+  onUpdate(event: Pizza) {}
 
   onRemove(event: Pizza) {
-    const remove = window.confirm('Are you sure?');
+    const remove = window.confirm("Are you sure?");
     if (remove) {
-
     }
   }
 }
